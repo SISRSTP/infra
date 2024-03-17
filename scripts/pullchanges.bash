663c83b8c6be1726cca46909e6819b66f18f8b67
NecroPrism4 <alexhid14@gmail.com>
2024-03-17 11:20:15 -0700

    commit

---
+++ scripts/pullchanges.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Stop script if an error occurs
+set -e
+
+# This script is used to pull the latest changes from the remote repository
+REPOSITORIES=("bustruck-service" "users-service" "Tramoove_app" "infra")
+
+
+for REPOSITORY in "${REPOSITORIES[@]}"
+# Iterate over each repository and do git fetch then git pull
+do
+    (
+    # Change directory to the repository directory
+    cd "../../$REPOSITORY"
+    # Fetch the latest changes
+    git fetch
+    # Pull the latest changes
+    git pull
+    echo "Repository $REPOSITORY updated successfully."
+    )
+done
